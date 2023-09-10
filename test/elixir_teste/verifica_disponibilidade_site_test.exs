4b4d3234389d8eabaa68f81cf35fdf1e2e0576f8
cassfer <cassiohafernandes@gmail.com>
2023-09-10 01:58:05 -0300

    10-09-2023 - Testes atualizados com mock

--- test/elixir_teste/verifica_disponibilidade_site_test.exs
+++ test/elixir_teste/verifica_disponibilidade_site_test.exs
@@ -1,5 +1,6 @@
 defmodule ElixirTesteTest.VerificaDisponibilidadeSiteTest do
 	use ExUnit.Case
+	import Mock
 
 	test "Retorna 200 caso o site esteja online" do
 		ElixirTeste.VerificaDisponibilidadeSite.verifica_primesoftware
@@ -12,5 +13,24 @@ defmodule ElixirTesteTest.VerificaDisponibilidadeSiteTest do
 		resultado = String.contains?(conteudo_arquivo, "200");
 
 		assert resultado == :true
+	end
+
+	test "retorna 200 com mock" do
+
+		:ets.new(:conteudos, [:set, :private, :named_table])
+
+		with_mock File, [write!: fn(_path, conteudo) -> :ets.insert_new(:conteudos, {conteudo}) end] do
+			ElixirTeste.VerificaDisponibilidadeSite.verifica_primesoftware
+		end
+
+		conteudos = :ets.tab2list(:conteudos)
+		[primeiro_valor | _] = conteudos
+
+		primeiro_valor_em_string = Kernel.inspect(primeiro_valor)
+
+		resultado = String.contains?(primeiro_valor_em_string, "200");
+
+		assert resultado == :true
+
 	end
 end
